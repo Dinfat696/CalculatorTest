296696082855ff55e47a00cbb89654f3e9f5c39d
dinarfathrahmanov <dinfat@mail.ru>
2023-06-12 16:31:22 +0300

    first commit

---
+++ src/main/java/com/example/calctets/CalcTetsApplication.java
@@ -0,0 +1,13 @@
+package com.example.calctets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalcTetsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalcTetsApplication.class, args);
+    }
+
+}
